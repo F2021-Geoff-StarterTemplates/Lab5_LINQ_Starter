7a01c8e4af0a659a6fa1f31e1745aa2f2ba9ce9e
GeoffGillespie <GeoffGillespie@users.noreply.github.com>
2025-02-17 15:14:55 -0400

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Lab5_LINQ.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Lab5_LINQ_Starter;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+        using (var context = new NorthwindContext())
+        {
+            //Load all DbSets
+            context.Products.Load();
+            context.Categories.Load();
+            context.Suppliers.Load();
+
+            // Cast all loaded data as collections
+            var products = context.Products.Local.ToObservableCollection();
+            var categories = context.Categories.Local.ToObservableCollection();
+            var suppliers = context.Suppliers.Local.ToObservableCollection();
+
+            // Instantiate a new MainWindow and pass it all the loaded data
+            // (Could also pass to other parts of the application, ie. usercontrol, page, etc.)
+            
+            var mainWindow = new MainWindow
+            {
+                DataContext = new
+                {
+                    Products = products,
+                    Categories = categories,
+                    Suppliers = suppliers
+                }
+            };
+            //Show the mainwindow object to the user.
+            //To avoid double windows, be sure to remove the StarupUri attribute in App.xaml
+            mainWindow.Show();
+        }
+    }
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace Lab5_LINQ_Starter;
+
+//Scaffold command used to generate the models/context for this lab
+//scaffold-dbcontext "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False" Microsoft.EntityFrameworkCore.SqlServer -outputdir Models/Generated -contextdir Data -namespace Lab5_LINQ.Models -contextnamespace Lab5_LINQ.Data -Tables Products, Categories, Suppliers -DataAnnotations -force
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+    }
+}
